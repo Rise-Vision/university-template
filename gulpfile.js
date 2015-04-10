7793d703355f13a05ef27fb3467cbebff327ebb2
Sylvianguy <sylvia.nguyen12@gmail.com>
2015-04-10 19:16:36 -0400

    gh pages

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+// ----------------------------
+// Include Dependencies 
+// ----------------------------
+
+var gulp            = require('gulp');
+var sass            = require('gulp-sass');
+
+
+// ----------------------------
+// Define Paths
+// ----------------------------
+
+var paths = {
+    sass_watch: './src/assets/styles/**/*.scss',
+    sass_main: './src/assets/styles/style.scss'
+};
+
+// ----------------------------
+// Define Gulp Sass Task
+// ----------------------------
+
+gulp.task('sass', function() {
+  gulp.src(paths.sass_main)
+    .pipe(sass())
+    .pipe(gulp.dest('./src/assets/styles/'))
+});
+
+
+
+// -------------------------
+// Set up our default tasks
+// ----------------------------
+
+gulp.task('default', function(){
+    gulp.start('sass')
+    gulp.watch(paths.sass_watch,['sass']); 
+}); 
